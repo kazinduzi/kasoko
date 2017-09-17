3e7c3c8f0176dd551c02c341bf133beb9b6aff96
kazinduzi <endayiragije@gmail.com>
2017-09-18 00:27:44 +0200

    Adding Unit Testing functionality

---
+++ application/front/tests/src/controllers/ProducControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Kazinduzi\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * Description of ProducControllerTest
+ *
+ * @author Emmanuel Ndayiragije <endayiragije@gmail.com>
+ */
+class ProducControllerTest extends TestCase
+{
+    /**
+     * 
+     */
+    public function testIndex()
+    {
+        $this->markTestSkipped('This controller method cannot be tested');
+    }
+    
+    
+}
